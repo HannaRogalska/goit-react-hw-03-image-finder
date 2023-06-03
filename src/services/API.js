8db575c97e1a2283321351d31d7cb7caa80eb6dd
Hanna Rogalska <hanrogalska@gmail.com>
2023-06-03 14:59:39 +0200

    instal loader

--- src/services/API.js
+++ src/services/API.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 const MY_KEY = '34923482-2fb4ccc15af9a628627f7d6c1';
 const API_URL = 'https://pixabay.com/api/';
 const axios = require('axios').default;
-
+ 
 async function FetchData(name, page, per_page) {
   const response = await axios.get(
-   
     `${API_URL}?key=${MY_KEY}&q=${name}&image_type=photo&orientation=horizontal&page=${page}&per_page=${per_page}`
   );
+   console.log(response)
   if (!response.data.total) {
     return Promise.reject(new Error(`No image with name ${name}`));
   }
+  console.log(response);
   return response.data;
 }
 
